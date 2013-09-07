fed7246adb3040182e2ad977d58d9ba73b3204fa
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2013-09-07 12:24:13 +0200

    added compiler for physics

---
+++ lib/physics/compile.py
@@ -0,0 +1,51 @@
+INPUT_FILES = []
+
+INPUT_FILES.append("jellyphysics.js")
+
+INPUT_FILES.append("queue.js")
+
+INPUT_FILES.append("bitmask.js")
+
+INPUT_FILES.append("internalspring.js")
+
+INPUT_FILES.append("jellyprerequisites.js")
+INPUT_FILES.append("vector2.js")
+INPUT_FILES.append("pointmass.js")
+INPUT_FILES.append("aabb.js")
+INPUT_FILES.append("vectortools.js")
+INPUT_FILES.append("closedshape.js")
+
+INPUT_FILES.append("body.js")
+INPUT_FILES.append("bodycollisioninfo.js")
+INPUT_FILES.append("materialmanager.js")
+INPUT_FILES.append("world.js")
+INPUT_FILES.append("springbody.js")
+INPUT_FILES.append("pressurebody.js")
+
+INPUT_FILES.append("ray.js")
+
+INPUT_FILES.append("bodybuilder.js")
+INPUT_FILES.append("bodyblueprint.js")
+INPUT_FILES.append("bodyfactory.js")
+INPUT_FILES.append("particle.js")
+INPUT_FILES.append("particlecannon.js")
+
+INPUT_FILES.append("joints/distancejoint.js")
+INPUT_FILES.append("joints/pinjoint.js")
+INPUT_FILES.append("joints/interpolationjoint.js")
+
+INPUT_FILES.append("springbuilder.js")
+
+INPUT_FILES.append("debugdraw.js")
+
+INPUT_FILES.append("contactmanager.js")
+
+INTRO_FILE = "intro.js"
+OUTRO_FILE = "outro.js"
+
+
+def main():
+	pass
+
+if __name__ == "__main__":
+        main()
