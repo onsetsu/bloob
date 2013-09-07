dc35d9d1dfe29201601d7a767d0d72280dc15ee5
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2013-09-07 17:21:30 +0200

    added compiler python script to minify physics to a single file

--- lib/physics/compile.py
+++ lib/physics/compile.py
@@ -36,15 +36,27 @@ INPUT_FILES.append("joints/interpolationjoint.js")
 
 INPUT_FILES.append("springbuilder.js")
 
-INPUT_FILES.append("debugdraw.js")
-
 INPUT_FILES.append("contactmanager.js")
 
-WRAPPER_FILE = "wrap.js"
+INPUT_FILES.append("quadtree.js")
 
+WRAPPER_FILE = "wrap.js"
+REPLACE_TEXT = "// CODE HERE"
+
+TARGET_FILE = "physics.js"
+
+OUTPUT = ""
 
 def main():
-	pass
+        global OUTPUT
+        with open(WRAPPER_FILE, "r") as wrapper:
+                OUTPUT += wrapper.read()
+        for inputFile in INPUT_FILES:
+                with open(inputFile, "r") as input:
+                        OUTPUT = OUTPUT.replace(REPLACE_TEXT, input.read() + REPLACE_TEXT)
+        OUTPUT = OUTPUT.replace(REPLACE_TEXT, "")
+        with open(TARGET_FILE, "w") as target:
+                target.write(OUTPUT)
 
 if __name__ == "__main__":
         main()
