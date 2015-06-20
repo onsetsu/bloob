15ab9f192f4f1f17a0d5a2f7e1e1781a029b39a1
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-06-20 16:04:59 +0200

    Lint gulpfile itself

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,13 @@
 var gulp = require('gulp');
 
-gulp.task('default', function () {
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+
+
+gulp.task('lint', function () {
+  return gulp.src('gulpfile.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('jshint-stylish'));
 });
+
+gulp.task('default', ['lint']);
