12ce23712766a9636cc8e8dbf3125ead16baa005
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-06-22 13:02:55 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 
-var shell = require('gulp-shell');
+//var shell = require('gulp-shell');
 
 gulp.task('lint', function () {
   return gulp.src('gulpfile.js')
@@ -12,6 +12,6 @@ gulp.task('lint', function () {
     .pipe(jshint.reporter('fail'));
 });
 
-gulp.task('jsdoc', shell.task(['./node_modules/jsdoc/jsdoc .']));
+//gulp.task('jsdoc', shell.task(['./node_modules/jsdoc/jsdoc .']));
 
-gulp.task('default', ['lint', 'jsdoc']);
+gulp.task('default', ['lint']);
