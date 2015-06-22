2ee9c168b9f339186af81b1fa1613134d57edb24
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-06-22 09:48:15 +0200

    Restyling

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,10 @@ var gulp = require('gulp');
 
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
-
-     if(   jshint === stylish)    {
-       
-       3 + 42     }
-       
-       i = 2
-  while(i++ <= 10)
-  {
-    17+42;
-    
-    
-    
-          }
           
 gulp.task('lint', function () {
   return gulp.src('gulpfile.js')
-    .pipe(jshint()) // '.jshintrc'
+    .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'))
     .pipe(jshint.reporter('fail'));
 });
