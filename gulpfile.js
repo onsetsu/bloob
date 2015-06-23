1d03a41e06675e07691bc33e79a78c46b02833b7
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-06-23 11:31:18 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ gulp.task('lint', function () {
 
 // Run unit tests
 gulp.task('test', function() {
-  return gulp.src('./test/**/*.js')
+  return gulp.src('./**/*.js')
     .pipe(karma({ configFile: 'test/karma.conf.js' }))
     .on('error', function(err) { throw err; });
 });
