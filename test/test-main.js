855a6626196bb2eaa79afb66f89919a70a14d2b5
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-06-26 14:55:41 +0200

    include devDependencies

--- test/test-main.js
+++ test/test-main.js
@@ -1,1 +1,33 @@
+var allTestFiles = [];
+var TEST_REGEXP = /test\.js$/;
 
+Object.keys(window.__karma__.files).forEach(function(file) {
+  if (TEST_REGEXP.test(file)) {
+    // Normalize paths to RequireJS module names.
+    allTestFiles.push(file);
+  }
+});
+
+require.config({
+  // Karma serves files under /base, which is the basePath from your config file
+  baseUrl: '/base/src',
+
+  // example of using a couple path translations (paths), to allow us to refer to different library dependencies, without using relative paths
+  paths: {
+    'jquery': '../lib/jquery',
+    'underscore': '../lib/underscore',
+  },
+
+  // example of using a shim, to load non AMD libraries (such as underscore)
+  shim: {
+    'underscore': {
+      exports: '_'
+    }
+  },
+
+  // dynamically load all test files
+  deps: allTestFiles,
+
+  // we have to kickoff jasmine, as it is asynchronous
+  callback: window.__karma__.start
+});
