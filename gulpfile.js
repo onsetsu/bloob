a5f98515ccaadf48a758c1ab19ec65f97ef002f1
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-06-27 17:05:31 +0200

    hacking gulp-karma

--- gulpfile.js
+++ gulpfile.js
@@ -17,19 +17,19 @@ gulp.task('lint', function () {
 
 // Run unit tests
 gulp.task('test', function(done) {
-//  return gulp.src(
+  return gulp.src('./idontexist'
 //  [
 //    //'./lib/external/require.js',
 //    './test/**/*.js',
 //    './test/test-main.js'
 //  ]
-//  )
-//    .pipe(karma({ configFile: 'test/karma.conf.js' }))
-//    .on('error', function(err) { throw err; });
-  karmaServer.start({
-    configFile: 'test/karma.conf.js',
-    singleRun: true
-  }, done);
+  )
+    .pipe(karma({ configFile: 'test/karma.conf.js' }))
+    .on('error', function(err) { throw err; });
+//  karmaServer.start({
+//    configFile: 'test/karma.conf.js',
+//    singleRun: true
+//  }, done);
 });
 
 gulp.task('default', ['lint', 'test']);
