2b3500ac4ed30ac5eca351f9413177a9e1895323
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-07-14 15:45:44 +0200

    test VectorTools

---
+++ spec-vector-test.js
@@ -0,0 +1,41 @@
+/*
+describe('MetaTest', function () {
+
+
+	describe('Should work', function () {
+
+		beforeEach(function () {
+			
+		});
+
+		it('42 should be 42', function () {
+			expect(42).toBe(42);
+		});
+		
+		it('sqr should abs negative values', function() {
+			expect(sqr(-42)).toBe(42);
+		});
+
+	});
+
+});
+*/
+console.log('spec-vector-test: before define');
+define(['./lib/num/vectortools.js'], function(VectorTools) {
+console.log('spec-vector-test: in define');
+
+    describe('AMD Test', function() {
+
+        it('VectorTool Test', function() {
+			var num = VectorTools.degToRad(
+				VectorTools.radToDeg(
+					42
+				)
+			);
+			expect(num).toBe(42);
+        });
+
+    });
+
+});
+console.log('spec-vector-test: after define');
