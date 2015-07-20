085c333b79c97a3962d898bd36cc90cea2a81b90
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2015-07-20 15:06:32 +0200

    refine test for Entity tags

--- test/engine/map/entity-test.js
+++ test/engine/map/entity-test.js
@@ -8,21 +8,13 @@ define(['./../../../lib/engine/map/entity'], function(Entity) {
 			blockEntity.addTag('moving');
 			blockEntity.addTag('entity');
 
-			console.log('block has tag moving', blockEntity.hasTag('moving'));
 			expect(blockEntity.hasTag('moving')).toBe(true);
-			console.log('block has tag entity', blockEntity.hasTag('entity'));
 			expect(blockEntity.hasTag('entity')).toBe(true);
 
 			blockEntity.removeTag('moving');
 
-			console.log('block should not have tag moving', blockEntity.hasTag('moving'));
 			expect(blockEntity.hasTag('moving')).toBe(false);
-			console.log('block has tag entity', blockEntity.hasTag('entity'));
 			expect(blockEntity.hasTag('entity')).toBe(true);
-
-			console.log(blockEntity.getTags());
-			expect(blockEntity.hasTag('moving')).toBe(false);
-			expect(blockEntity.hasTag('entity')).toBe(false);
 		});
 
 	});
