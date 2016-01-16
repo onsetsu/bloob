65510fb0ce16bbd690af1ebc0a2d650a63fbfef7
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2016-01-16 10:15:33 +0100

    run full Entity test

--- test/engine/map/entity-test.js
+++ test/engine/map/entity-test.js
@@ -4,7 +4,6 @@ define(['./../../../lib/engine/map/entity'], function(Entity) {
 
 		it('should carry attached tags', function() {
 			var blockEntity = new Entity('testBlock');
-			return;
 
 			blockEntity.addTag('moving');
 			blockEntity.addTag('entity');
